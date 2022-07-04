ee58aded95674684baaf5180ed705d1dba20ae0c
yjx <1104675407@qq.com>
2022-07-04 23:20:32 +0800

    second commit by yjx

--- src/main/java/com/h3c/GitTest.java
+++ src/main/java/com/h3c/GitTest.java
@@ -11,5 +11,6 @@ package com.h3c;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello ...");
+        System.out.println("hello,xiaoxiao");
     }
 }
