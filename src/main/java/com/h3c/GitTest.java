5b012520a5d1432f95e4f0c156bf1b292a04b5f8
yjx <1104675407@qq.com>
2022-07-04 23:18:01 +0800

    first commit by yjx

---
+++ src/main/java/com/h3c/GitTest.java
@@ -0,0 +1,15 @@
+package com.h3c;
+
+/**
+ * className:GitTest
+ * package:com.h3c
+ * description:
+ *
+ * @Date:2022/7/422:55
+ * @Author:y17887
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello ...");
+    }
+}
